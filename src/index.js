50642c7c509ae07cf455a849bf6e201030fe16a5
P7XXTMX <39622042+P7XXTMX@users.noreply.github.com>
2025-11-10 21:58:12 +0800

    123

---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+export class MyDurableObject {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request) {
+    // 解析请求内容或参数
+    const url = new URL(request.url);
+
+  
+     // 目标 Emby 服务器的 URL
+	  const targetUrl = 'https://random.ouonet.org';
+
+	  // 构造新的请求，保留原始请求的 URL 路径和查询参数
+	  const url = new URL(request.url);
+	  const newUrl = targetUrl + url.pathname + url.search;
+
+    // 传递请求方法、头部等
+  const newRequest = new Request(newUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+      redirect: 'follow'
+  });
+
+  // 发出请求到目标 Emby 服务器
+  const response = await fetch(newRequest);
+ 
+
+    // 返回响应
+  return response;
+  }
+}
+
+export default {
+  async fetch(request, env) {
+    // 获取或创建 Durable Object 实例
+    const id = env.MY_DO_NAMESPACE.idFromName("singleton");
+    const obj = env.MY_DO_NAMESPACE.get(id);
+
+    // 向 Durable Object 发起fetch，实现请求下发
+    const doResponse = await obj.fetch(request);
+
+    return doResponse;
+  }
+}
